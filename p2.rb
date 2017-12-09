c17a6604101161205a355086364cfbf1411adf6b
JakeFrey <jake.frey94@gmail.com>
2017-12-09 11:24:31 -0800

    smaller p2

--- p2.rb
+++ p2.rb
@@ -2,10 +2,10 @@ a = eval(gets.chomp)
 b = eval(gets.chomp)
 c = []
 for r in b
-  for n in a
-    if n.between?(r[0], r[1])
-      c.push(n)
-    end
-  end
+	for n in a
+		if n.between?(r[0], r[1])
+			c.push(n)
+		end
+	end
 end
 p c
